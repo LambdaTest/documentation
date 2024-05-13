318216bfbc2c580361c0b161213bc6a19348d244
ishavyas9 <ishav@lambdatest.com>
2024-05-13 20:35:53 +0530

    Doc bottom cta

--- src/theme/DocPaginator.js
+++ src/theme/DocPaginator.js
@@ -14,7 +14,20 @@ export default function DocPaginator(props) {
   }, [])
   return (
     <>
-    {showFeedback && <HelpFulArticle />}
+    {showFeedback && 
+    <>
+    <HelpFulArticle />
+    
+    <div className="Doc_bottom_Cta">
+       <div className="Doc_cta">
+        <div className='right_sec'>
+          <h2>Test across 3000+ combinations of browsers, real devices & OS.</h2>
+          <a href="https://www.lambdatest.com/demo" target="_blank">Book a Demo</a>
+        </div>
+        <img loading="lazy" src="https://www.lambdatest.com/blog/wp-content/uploads/2024/05/DocBottomImg.png" alt="Book Demo" title="Book Demo" width="887" height="477" className='left_sec'></img>
+       </div>
+    </div></>
+}
     <div className="social_button">
         <button onClick={() => window.openLTChatWidget()}  className="chat_btn"><img loading="lazy" src="/support/img/Chat.svg" alt=" " role="presentation" title='Chat with Us' className='' width="16" height="16"/>Chat with Us</button>
         <a role="button" tabindex="0" href="https://community.lambdatest.com/" target="_blank"  className="btnlink">Ask the Community</a>
