73449b33662655fe9018e20b7f1c7424c982f31b
amanchopra1905 <amanc@lambdatest.com>
2024-06-21 12:53:21 +0530

    xctestplan doc updated

--- sidebars.js
+++ sidebars.js
@@ -2289,9 +2289,13 @@ module.exports = {
         label: "Speedup XCUI Tests",
         items: [
           "sharding-xcui",
-          "xctestplan",
-          "filters-xcui",
+          "speedup-xcui",
         ],
+      },
+      {
+        type: "doc",
+        label: "XCTestPlan",
+        id: "xctestplan",
       },
       {
         type: "doc",
