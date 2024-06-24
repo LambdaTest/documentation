06939ed3b54f5ee5225a92c5ca02a1ee89e6be5c
shubhamsuri <shubhamsuri@lambdatest.com>
2024-06-24 13:19:34 +0530

    changed theme according to prism IN-8497

--- docs/getting-started.md
+++ docs/getting-started.md
@@ -19,6 +19,9 @@ site_name: LambdaTest
 slug: getting-started-with-lambdatest-automation/
 ---
 
+import SupportedLanguage from '../src/component/SupportedLanguage';
+
+
 <script type="application/ld+json"
       dangerouslySetInnerHTML={{ __html: JSON.stringify({
        "@context": "https://schema.org",
@@ -53,167 +56,11 @@ LambdaTest Selenium Automation Grid enables you to perform end-to-end automation
 
 Here are all the languages and frameworks supported by LambdaTest online Selenium Grid.
 
-<div className="lt_row lt_framework_list_row">
-    <div className="lt_col lt_framework_wrapper"> 
-      <img loading="lazy" src={require('../assets/images/getting-started/Java.png').default} alt="Java" width="28" height="28" className="language-icon"/>
-      <ul className="lt_framework_list">
-        <li>
-          <a className="lt_primary" href="/docs/java-with-selenium-running-java-automation-scripts-on-lambdatest-selenium-grid/">Java</a>
-        </li>
-        <li>
-          <a href="/docs/selenide-tests-with-lambdatest-online-selenium-grid-for-automated-cross-browser-testing/">Selenide</a>
-        </li>
-        <li>
-          <a href="/docs/running-gauge-tests-on-lambdatest-selenium-grid/">Gauge</a>
-        </li>
-        <li>
-          <a href="/docs/testng-with-selenium-running-java-automation-scripts-on-lambdatest-selenium-grid/">TestNG</a>
-        </li>
-        <li>
-          <a href="/docs/run-geb-tests-on-selenium-grid/">Geb</a>
-        </li>
-        <li>
-          <a href="/docs/junit-with-selenium-running-junit-automation-scripts-on-lambdatest-selenium-grid/">JUnit</a>
-        </li>
-        <li>
-          <a href="/docs/running-cucumber-scripts-with-testng-and-selenium/">Cucumber</a>
-        </li>
-        <li>
-          <a href="/docs/serenity-test-on-selenium-grid/">Serenity BDD</a>
-        </li>
-      </ul>
-    </div>
-    <div className="lt_col lt_framework_wrapper">
-      <img loading="lazy" src={require('../assets/images/getting-started/Php.png').default} alt="PHP" width="28" height="28" className="language-icon black_logo"/>
-      <ul className="lt_framework_list">
-        <li>
-          <a className="lt_primary" href="/docs/php-with-selenium-running-php-automation-scripts-on-lambdatest-selenium-grid/">PHP</a>
-        </li>
-        <li>
-          <a href="/docs/laravel-dusk-with-selenium-running-laravel-dusk-automation-scripts-on-lambdatest-selenium-grid/">Laravel</a>
-        </li>
-        <li>
-          <a href="/docs/codeception-integration-with-lambdatest/">Codeception</a>
-        </li>
-        <li>
-          <a href="/docs/phpunit-with-selenium-running-phpunit-automation-scripts-on-lambdatest-selenium-grid/">PHPUnit</a>
-        </li>
-        <li>
-          <a href="/docs/behat-with-selenium-running-behat-automation-scripts-on-lambdatest-selenium-grid/">Behat</a>
-        </li>
-      </ul>
-    </div>
-    <div className="lt_col lt_framework_wrapper">
-       <img loading="lazy" src={require('../assets/images/getting-started/Ruby.png').default} alt="Ruby" width="28" height="28" className="language-icon"/>
-      <ul className="lt_framework_list">
-        <li>
-          <a className="lt_primary" href="/docs/ruby-with-selenium-running-ruby-automation-scripts-on-lambdatest-selenium-grid/">Ruby</a>
-        </li>
-        <li>
-          <a href="/docs/rspec-with-selenium-running-rspec-automation-scripts-on-lambdatest-selenium-grid/">RSpec</a>
-        </li>
-        <li>
-          <a href="/docs/testunit-with-selenium-running-testunit-automation-scripts-on-lambdatest-selenium-grid/">Test::Unit</a>
-        </li>
-        <li>
-          <a href="/docs/ruby-with-capybara-running-automation-scripts-on-lambdatest-selenium-grid/">Capybara</a>
-        </li>
-      </ul>
-    </div>
-    <div className="lt_col lt_framework_wrapper">
-     <img loading="lazy" src={require('../assets/images/getting-started/C-Sharp.png').default} alt="C#" width="28" height="28" className="language-icon"/>
-      <ul className="lt_framework_list">
-        <li>
-          <a className="lt_primary" href="/docs/c-with-selenium-running-c-automation-scripts-on-lambdatest-selenium-grid/">C#</a>
-        </li>
-        <li>
-          <a href="/docs/specflow-with-selenium-running-specflow-automation-scripts-on-lambdatest-selenium-grid/">SpecFlow</a>
-        </li>
-        <li>
-          <a href="/docs/mstest-with-selenium-running-mstest-automation-scripts-on-lambdatest-selenium-grid/">MSTest</a>
-        </li>
-        <li>
-          <a href="/docs/nunit-with-selenium-running-nunit-automation-scripts-on-lambdatest-selenium-grid/">NUnit</a>
-        </li>
-         <li>
-          <a href="/docs/selenium-with-xunit/">xUnit</a>
-        </li>
-      </ul>
-    </div>
-    <div className="lt_col lt_framework_wrapper">
-      <img loading="lazy" src={require('../assets/images/getting-started/Python.png').default} alt="Python" width="28" height="28" className="language-icon"/>
-      <ul className="lt_framework_list">
-         <li>
-          <a className="lt_primary" href="/docs/python-with-selenium-running-python-automation-scripts-on-lambdatest-selenium-grid/">Python</a>
-        </li>
-        <li>
-          <a href="/docs/pytest-with-selenium-running-pytest-automation-script-on-lambdatest-selenium-grid/">Pytest</a>
-        </li>
-        <li>
-          <a href="/docs/behave-with-selenium-running-behave-automation-scripts-on-lambdatest-selenium-grid/">Behave</a>
-        </li>
-        <li>
-          <a href="/docs/robot-with-selenium-running-robot-automation-scripts-on-lambdatest-selenium-grid/">Robot</a>
-        </li>
-        <li>
-          <a href="/docs/running-unit-testing-in-python-on-lambdatest-selenium-grid/">Unittest</a>
-        </li> 
-         <li>
-          <a href="/docs/automation-testing-selenium-with-lettuce/">Lettuce</a>
-        </li> 
-      </ul>
-    </div>
-    <div className="lt_col lt_framework_wrapper">
-       <img loading="lazy" src={require('../assets/images/getting-started/Node-JS.png').default} alt="Javascript" width="28" height="28" className="language-icon"/>
-      <ul className="lt_framework_list">
-        <li>
-          <a className="lt_primary" href="/docs/javascript-with-selenium-running-javascript-automation-scripts-on-lambdatest-selenium-grid/">JavaScript</a>
-        </li>
-        <li>
-          <a href="/docs/webdriverio-5-6-2-with-selenium-running-automation-scripts-on-lambdatest-selenium-grid/">WebDriverIO 5.6.2</a>
-        </li>
-        <li>
-          <a href="/docs/nightwatch-with-selenium-running-nightwatch-automation-scripts-on-lambdatest-selenium-grid/">NightwatchJS</a>
-        </li>
-        <li>
-          <a href="/docs/webdriverio-with-selenium-running-webdriverio-automation-scripts-on-lambdatest-selenium-grid/">WebDriverIO</a>
-        </li>
-        <li>
-          <a href="/docs/automation-testing-with-mocha-and-selenium/">Mocha</a>
-        </li>
-        <li>
-          <a href="/docs/wd-with-selenium-running-wd-automation-scripts-on-lambdatest-selenium-grid/">WD</a>
-        </li>
-        <li>
-          <a href="/docs/automation-testing-with-selenium-and-jest/">Jest Library</a>
-        </li>
-        <li>
-          <a href="/docs/angularjs-with-karma-running-angular-tests-on-lambdatest-selenium-grid/">AngularJS With Karma</a>
-        </li>
-        <li>
-          <a href="/docs/cucumberjs-with-selenium-running-cucumberjs-automation-scripts-on-lambdatest-selenium-grid/">CucumberJS For BDD</a>
-        </li>
-        <li>
-          <a href="/docs/jasmine-with-karma-running-jasmine-tests-on-lambdatest-selenium-grid/">Jasmine For BDD</a>
-        </li>
-        <li>
-          <a href="/docs/karma-integration-with-lambdatest/">Karma Test Runner</a>
-        </li>
-        <li>
-          <a href="/docs/npm-plugin-for-testcafe-integration-with-lambdatest/">TestCafe</a>
-        </li>
-         <li>
-          <a href="/docs/selenium-tests-with-nemojs/">NemoJS</a>
-        </li>
-         <li>
-          <a href="/docs/protractor-with-selenium-running-protractor-automation-scripts-on-lambdatest-selenium-grid/">Protractor</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div className="lt-framework-list-footer">
-    <p>We support all languages and frameworks that are compatible with Selenium, so in case your favorite isn't in the table.<br/>Don't worry, you can still run the test. <span className="doc__lt" onClick={() => window.openLTChatWidget()}>Contact Us</span> for any help.</p>
-  </div>
+<SupportedLanguage />
+
+:::info
+We support all languages and frameworks that are compatible with Selenium, so in case your favorite isn't in the table.<br/>Don't worry, you can still run the test. <span className="doc__lt" onClick={() => window.openLTChatWidget()}>Contact Us</span> for any help.
+:::
 
 ## Hybrid Frameworks Support
 
--- src/component/HelpFulArticle.js
+++ src/component/HelpFulArticle.js
@@ -102,39 +102,82 @@ export default function HelpFulArticle() {
     }, [isContactFormSubmitted])
 
     return (
-        <section>
             <div className="feedback__box">
-                {!feedbackSubmitted && !isContactFormSubmitted && <div>
-                    {feedback !== "Negative" && <div>
-                    <h3 className='feedback__box__title feedback__box__title_helpful'>
-                        Was this page helpful?
-                        <div className='feedback__box__icons'>
-                            <button role="button"  tabindex="0" type="button" onClick={() => { feedbackSubmit() }} title='Document Feedback' aria-label="Document Feedback" >
-                                {feedback == "Positive" ? 
-                                <svg className='filled_svg' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M4 21H5V8H4C3.46957 8 2.96086 8.21071 2.58579 8.58579C2.21071 8.96086 2 9.46957 2 10V19C2 19.5304 2.21071 20.0391 2.58579 20.4142C2.96086 20.7893 3.46957 21 4 21ZM20 8H13L14.122 4.632C14.2221 4.33141 14.2494 4.01134 14.2016 3.69815C14.1538 3.38496 14.0322 3.08761 13.847 2.8306C13.6617 2.57358 13.4181 2.36424 13.1361 2.21984C12.8541 2.07543 12.5418 2.00008 12.225 2H12L7 7.438V21H18L21.937 12.351L22 12.058V10C22 9.46957 21.7893 8.96086 21.4142 8.58579C21.0391 8.21071 20.5304 8 20 8Z" fill="#0569FF"/>
+                
+                <div className='support_container'>
+                
+                    <div>
+                        <h3 className='feedback__box__title feedback__box__title_helpful'>Do you find this helpful?</h3>
+                    {!feedbackSubmitted && !isContactFormSubmitted &&
+                       <div className='feedback__box__icons'>
+                            <button role="button"  tabIndex="0" type="button" onClick={() => { feedbackSubmit() }} title='Document Feedback' aria-label="Document Feedback" >
+                                <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g filter="url(#filter0_d_20381_3252)">
+                                        <g clipPath="url(#clip0_20381_3252)">
+                                            <rect width="32" height="32" rx="6" fill="#F7F7F7" />
+                                            <path d="M17.4688 13.2816L17.2831 13.9211H17.949H21.55C21.9189 13.9211 22.3333 14.291 22.3333 14.8947V16.3684V16.3691C22.3334 16.4058 22.3277 16.4408 22.3174 16.4722L20.549 21.8869C20.5489 21.8871 20.5489 21.8874 20.5488 21.8876C20.4842 22.0827 20.3733 22.2398 20.2411 22.3449L20.2411 22.345C20.1099 22.4493 19.9633 22.4992 19.8207 22.5H11.2833C10.9144 22.5 10.5 22.1301 10.5 21.5263V14.8947C10.5 14.291 10.9144 13.9211 11.2833 13.9211H12.9074H13.1623L13.3121 13.7147L16.327 9.56007C16.3271 9.55994 16.3272 9.55981 16.3273 9.55969C16.3663 9.50644 16.403 9.5 16.4167 9.5H17.451C17.6708 9.5 17.9095 9.62338 18.0696 9.87809C18.2315 10.136 18.2808 10.4845 18.1892 10.8011L17.4688 13.2816Z" stroke="#666666" />
+                                        </g>
+                                        <rect x="0.5" y="0.5" width="31" height="31" rx="5.5" stroke="#D4D4D4" />
+                                    </g>
+                                    <defs>
+                                        <filter id="filter0_d_20381_3252" x="0" y="0" width="32" height="33" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                            <feOffset dy="1" />
+                                            <feColorMatrix type="matrix" values="0 0 0 0 0.121569 0 0 0 0 0.121569 0 0 0 0 0.121569 0 0 0 0.04 0" />
+                                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_20381_3252" />
+                                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_20381_3252" result="shape" />
+                                        </filter>
+                                        <clipPath id="clip0_20381_3252">
+                                            <rect width="32" height="32" rx="6" fill="white" />
+                                        </clipPath>
+                                    </defs>
                                 </svg>
-                                : 
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M20 8H14.388L15.511 4.633C15.713 4.025 15.611 3.351 15.236 2.831C14.861 2.311 14.253 2 13.612 2H12C11.703 2 11.422 2.132 11.231 2.36L6.531 8H4C2.897 8 2 8.897 2 10V19C2 20.103 2.897 21 4 21H17.307C17.7139 20.9986 18.1108 20.8738 18.4452 20.6421C18.7797 20.4103 19.0359 20.0825 19.18 19.702L21.937 12.351C21.9789 12.2387 22.0002 12.1198 22 12V10C22 8.897 21.103 8 20 8ZM4 10H6V19H4V10ZM20 11.819L17.307 19H8V9.362L12.468 4H13.614L12.052 8.683C12.0013 8.83332 11.9871 8.99355 12.0107 9.15043C12.0343 9.3073 12.095 9.45629 12.1877 9.58504C12.2803 9.71379 12.4024 9.8186 12.5436 9.89076C12.6849 9.96293 12.8414 10.0004 13 10H20V11.819Z" fill="black"/>
-                                </svg>
-                                }
+
                             </button>
-                            <button role="button" tabindex="0" type="button" onClick={() => { SetFeedback("Negative") }} title='Document Feedback' aria-label="Document Feedback">
-                                {feedback == "Negative" ? 
-                                <svg className='filled_svg' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M20 3H19V16H20C20.5304 16 21.0391 15.7893 21.4142 15.4142C21.7893 15.0391 22 14.5304 22 14V5C22 4.46957 21.7893 3.96086 21.4142 3.58579C21.0391 3.21071 20.5304 3 20 3ZM4 16H11L9.878 19.368C9.77789 19.6686 9.75061 19.9887 9.79843 20.3018C9.84624 20.615 9.96778 20.9124 10.153 21.1694C10.3383 21.4264 10.5819 21.6358 10.8639 21.7802C11.1459 21.9246 11.4582 21.9999 11.775 22H12L17 16.562V3H6L2.063 11.649L2 11.942V14C2 14.5304 2.21071 15.0391 2.58579 15.4142C2.96086 15.7893 3.46957 16 4 16Z" fill="#0569FF"/>
+                            <button role="button" tabIndex="0" type="button" onClick={() => { SetFeedback("Negative") }} title='Document Feedback' aria-label="Document Feedback">
+                                <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g filter="url(#filter0_d_20381_3253)">
+                                        <g clipPath="url(#clip0_20381_3253)">
+                                            <rect width="32" height="32" rx="6" fill="#F7F7F7" />
+                                            <path d="M17.4688 18.7184L17.2831 18.0789H17.949H21.55C21.9189 18.0789 22.3333 17.709 22.3333 17.1053V15.6316V15.6309C22.3334 15.5942 22.3277 15.5592 22.3174 15.5278L20.549 10.1131C20.5489 10.1129 20.5489 10.1126 20.5488 10.1124C20.4842 9.91729 20.3733 9.76025 20.2411 9.65507L20.2411 9.65505C20.1099 9.55066 19.9633 9.50076 19.8207 9.5H11.2833C10.9144 9.5 10.5 9.86988 10.5 10.4737V17.1053C10.5 17.709 10.9144 18.0789 11.2833 18.0789H12.9074H13.1623L13.3121 18.2853L16.327 22.4399C16.3271 22.4401 16.3272 22.4402 16.3273 22.4403C16.3663 22.4936 16.403 22.5 16.4167 22.5H17.451C17.6708 22.5 17.9095 22.3766 18.0696 22.1219C18.2315 21.864 18.2808 21.5155 18.1892 21.1989L17.4688 18.7184Z" stroke="#666666" />
+                                        </g>
+                                        <rect x="0.5" y="0.5" width="31" height="31" rx="5.5" stroke="#D4D4D4" />
+                                    </g>
+                                    <defs>
+                                        <filter id="filter0_d_20381_3253" x="0" y="0" width="32" height="33" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                            <feOffset dy="1" />
+                                            <feColorMatrix type="matrix" values="0 0 0 0 0.121569 0 0 0 0 0.121569 0 0 0 0 0.121569 0 0 0 0.04 0" />
+                                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_20381_3253" />
+                                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_20381_3253" result="shape" />
+                                        </filter>
+                                        <clipPath id="clip0_20381_3253">
+                                            <rect width="32" height="32" rx="6" fill="white" />
+                                        </clipPath>
+                                    </defs>
                                 </svg>
-                                : 
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M4 16H9.612L8.489 19.367C8.287 19.975 8.389 20.649 8.764 21.169C9.139 21.689 9.747 22 10.388 22H12C12.297 22 12.578 21.868 12.769 21.64L17.469 16H20C21.103 16 22 15.103 22 14V5C22 3.897 21.103 3 20 3H6.693C6.28611 3.00135 5.88921 3.12616 5.55478 3.35792C5.22035 3.58969 4.96413 3.91749 4.82 4.298L2.063 11.649C2.02114 11.7613 1.9998 11.8802 2 12V14C2 15.103 2.897 16 4 16ZM20 14H18V5H20V14ZM4 12.181L6.693 5H16V14.638L11.532 20H10.386L11.948 15.317C11.9987 15.1667 12.0129 15.0064 11.9893 14.8496C11.9657 14.6927 11.905 14.5437 11.8123 14.415C11.7197 14.2862 11.5976 14.1814 11.4564 14.1092C11.3151 14.0371 11.1586 13.9996 11 14H4V12.181Z" fill="black"/>
-                                </svg>
-                                }
                             </button>
                         </div>
-                    </h3>
+                    }
+                    {feedbackSubmitted && <div className='feedback__box__thanks-title'>
+                        <h3>Thanks for your feedback!</h3>
                     </div>}
-                    {feedback == "Negative" &&
+                    {isContactFormSubmitted && <div className='feedback__box__thanks-title'>
+                        <h3>Thanks for your feedback!</h3>
+                    </div>}
+                    </div>
+
+                    <div className="social_button">
+                        <h3 className='feedback__box__title feedback__box__title_helpful'>Still need help?</h3>
+                        <button onClick={() => window.openLTChatWidget()} className="chat_btn"><img loading="lazy" src="/support/img/Chat.svg" alt=" " role="presentation" title='Chat with Us' className='' width="16" height="16" />Chat with Us</button>
+                        <a role="button" tabIndex="0" href="https://community.lambdatest.com/" target="_blank" className="chat_btn"><img loading="lazy" src="/support/img/community.svg" alt=" " role="presentation" title='Chat with Us' className='' width="16" height="16" />Ask the Community</a>
+                        {/* <a href="https://accounts.lambdatest.com/register" target="_blank"  className="btnlink">Start Free Testing</a> */}
+                    </div>
+                </div>
+
+            {feedback == "Negative" && !isContactFormSubmitted &&
                         <form ref={form} onSubmit={handleSubmit} className="feedback__box__form">
                             <p className='feedback__box__form__title'>We're sorry to hear that. Please share your feedback with us.</p>
                             <ol className="feedback__box__form__radios">
@@ -169,17 +212,8 @@ export default function HelpFulArticle() {
                             </div>
                         </form>
                     }
-                </div>}
-                {feedbackSubmitted && <div className='feedback__box__thanks-title'>
-                    <h3>Thanks for your feedback!</h3>
-                </div>}
-                {isContactFormSubmitted && <div className='feedback__box__thanks-title'>
-                    <h3>Thanks for your feedback!</h3>
-                </div>}
+                
+                
             </div>
-
-
-
-        </section>
     );
 }
