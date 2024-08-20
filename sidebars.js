c71a09a0d63139f5bc071fb0e966417da8a32e88
amanchopra1905 <amanc@lambdatest.com>
2024-08-20 11:20:54 +0530

    accessibility selenium hyp

--- sidebars.js
+++ sidebars.js
@@ -1419,6 +1419,11 @@ module.exports = {
             label: "HyperExecute",
             items: [
               {
+                type: "doc",
+                label: 'Selenium',
+                id: "selenium-hyprerexecute-accessibility-tests"
+              },
+              {
                 type: "category",
                 collapsed: true,
                 label: 'Cypress',
