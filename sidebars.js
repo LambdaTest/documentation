6768d76f353000d463e9c54705d6130635908e53
ShubhamSuri <70204532+surishubham@users.noreply.github.com>
2024-09-19 16:43:04 +0530

    Merge pull request #952 from amanchopra1905/stage
    updated the real time documentation

--- sidebars.js
+++ sidebars.js
@@ -1075,28 +1075,23 @@ module.exports = {
         collapsed: true,
         label: "Web Browser Testing",
         items: [
-          {
-            type: "category",
-            collapsed: false,
-            label: "Desktop Browser Testing",
-            items: [
-              'getting-started-with-desktop-browser-real-time-testing',
-            ]
-          },
-          {
-            type: "category",
-            collapsed: true,
-            label: "Virtual Mobile Testing",
-            items: [
-              'getting-started-with-mobile-browser-real-time-testing',
-            ]
-          }
+          'getting-started-with-desktop-browser-real-time-testing',
+          'real-time-desktop-browser-testing-tools'
         ]
       },
       {
         type: "category",
         collapsed: true,
-        label: "App Testing",
+        label: "Mobile Browser Testing",
+        items: [
+          'getting-started-with-mobile-browser-real-time-testing',
+          'real-time-mobile-browser-testing-tools'
+        ]
+      },
+      {
+        type: "category",
+        collapsed: true,
+        label: "Mobile App Testing",
         items: [
           'getting-started-with-mobile-app-real-time-testing',
         ]
