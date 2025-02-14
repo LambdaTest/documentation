831fd6ab68ad06fe919897e79b686abbb5c1c2f6
amanchopra1905 <amanc@lambdatest.com>
2025-02-14 19:01:00 +0530

    kane ai modules and version

--- sidebars.js
+++ sidebars.js
@@ -1119,7 +1119,7 @@ module.exports = {
           "kane-ai-command-guide",
           "kane-ai-web-test-writing-guidelines",
           "kaneai-upload-and-download-files",
-          // "kaneai-modules-versions-and-enhancement"
+          "kaneai-modules-versions-and-enhancement"
         ],
       },
       {
