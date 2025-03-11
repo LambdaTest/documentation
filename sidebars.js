c4f2e7cd1c5a2c6347434e4670ce917f86dca495
ShubhamSuri <70204532+surishubham@users.noreply.github.com>
2025-03-11 17:23:46 +0530

    Merge pull request #1400 from srivishnua-lambdatest/stage
    Insights - Adds documentation for Private Desktop Insights

--- sidebars.js
+++ sidebars.js
@@ -3027,6 +3027,7 @@ module.exports = {
     "analytics-allure-api-widgets",
     "analytics-test-case-insights",
     "insights-private-real-devices",
+    "insights-private-desktop",
     "insights-usage-report",
     "analytics-sub-organization-widgets",
     "analytics-dashboard-edit-layout",
