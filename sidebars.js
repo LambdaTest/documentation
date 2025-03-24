1538311329611de81b0c9f89c197bead47b09cb3
ShubhamSuri <70204532+surishubham@users.noreply.github.com>
2025-03-24 15:42:43 +0530

    Merge pull request #1444 from amanchopra1905/stage
    flutter for virtual device

--- sidebars.js
+++ sidebars.js
@@ -2411,8 +2411,15 @@ module.exports = {
       {
         type: "category",
         collapsed: true,
+        type: "doc",
         label: "Emulator / Simulator",
         items: ["app-automation-app-sim"],
+        id: "app-automation-app-sim",
+      },
+      {
+        type: "doc",
+        label: "Flutter",
+        id: "virtual-device-flutter-apps",
       },
     ],
   ],
