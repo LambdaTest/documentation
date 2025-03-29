28caf23627702c992258f6f5da7b194939c960ad
ShubhamSuri <70204532+surishubham@users.noreply.github.com>
2025-03-29 17:41:42 +0530

    Merge pull request #1469 from Private1647/stage
    Added Supported Assertions document

--- sidebars.js
+++ sidebars.js
@@ -1124,6 +1124,7 @@ module.exports = {
         items: [
           "kane-ai-command-guide",
           "kane-ai-web-test-writing-guidelines",
+          "kane-ai-supported-assertions",
         ],
       },
       {
