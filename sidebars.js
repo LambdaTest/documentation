f79d88fd603dfb909fb407471121be6221a9adee
Deepasnhu-product <deepanshum@lambdatest.com>
2025-06-26 19:47:21 +0530

    update detox doc

--- sidebars.js
+++ sidebars.js
@@ -339,6 +339,11 @@ module.exports = {
           },
           {
             type: "doc",
+            label: "Detox",
+            id: "detox-testing-on-hyperexecute",
+          },
+          {
+            type: "doc",
             label: "XCUI",
             id: "hyperexecute-xcui-testing",
           },
