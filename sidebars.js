f40e441117be88ae723793c5d95e812f8f364d47
Saksham Arora <sakshamarora@lambdatest.com>
2025-08-13 22:16:08 +0530

    Updated documentation

--- sidebars.js
+++ sidebars.js
@@ -2090,7 +2090,8 @@ module.exports = {
               "accessibility-settings-ios",
               "devtools-on-real-devices-app",
               "set-date-time-hour-format-real-devices",
-              "assistive-touch-on-real-ios-devices"
+              "assistive-touch-on-real-ios-devices",
+              "dark-mode-on-real-devices"
             ],
           },
         ],
@@ -2112,6 +2113,7 @@ module.exports = {
             "screen-reader-on-real-devices-browser",
             "ip-geolocation-on-real-devices-browser",
             "assistive-touch-on-real-ios-browser",
+            "dark-mode-on-browser",
             "contacts-on-real-devices-browser",
             "set-date-time-hour-format-real-devices-browser",
           ]
