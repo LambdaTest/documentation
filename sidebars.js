8bd349e72fc80e3df0171f8ac31c1defe9c1110c
Manuraj-7 <manurajs@lambdatest.com>
2025-08-13 15:36:20 +0530

    Audit Logs + Custom Status + Assertions Doc

--- sidebars.js
+++ sidebars.js
@@ -2014,9 +2014,10 @@ module.exports = {
         ],
       },
       {
-        type: "doc",
-        label: "Test Run",
-        id: "test-run-creation-and-management",
+        type: "category",
+        collapsed: true,
+        label: "Test Run & Instances",
+        items: ["test-run-creation-and-management", "test-instance-audit-logs"],
       },
       {
         type: "doc",
