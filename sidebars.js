f185afc4101f91552b5cc5e1bf0e0da2091aa270
Isha vyas <72408461+Ishavyas9@users.noreply.github.com>
2025-08-18 20:47:02 +0530

    Merge pull request #1976 from sakshamarora-arch/stage
    Updated documentation

--- sidebars.js
+++ sidebars.js
@@ -2859,6 +2859,7 @@ module.exports = {
               "appium-date-and-time",
               "device-passcode-appautomation",
               "set-device-dark-mode",
+              "auto-heal-appium"
             ],
           },
           {
