111fca08e5275a4ca427ae9bf649a8c917dd32de
Tejas Amle <tejasa@lambdatest.com>
2025-10-14 15:15:33 +0530

    new accessibility crawler doc

--- sidebars.js
+++ sidebars.js
@@ -1879,6 +1879,7 @@ module.exports = {
             items: [
               "accessibility-test-scheduling-scan",
               "accessibility-test-scheduling-edit",
+              "accessibility-test-crawler",
             ],
           },
         ],
