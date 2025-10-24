ae1e8eb1cd089ba6e190e526282d90ce7514f0bb
Shantanu Wali <shantanuw@lambdatest.com>
2025-10-24 15:36:40 +0530

    Mobile app advanced capabilities

--- sidebars.js
+++ sidebars.js
@@ -1304,6 +1304,7 @@ module.exports = {
           "kane-ai-jira-integration",
           "kane-ai-api-testing",
           "kane-ai-network-assertions",
+          "kane-ai-mobile-app-capabilities",
           "kaneai-network-throttling",
           "kane-ai-modules",
           "kaneai-modules-versions-and-enhancement",
