16cd04b5fa713907162b7440c4a1c9e177d63acd
Isha vyas <72408461+Ishavyas9@users.noreply.github.com>
2025-10-28 21:05:57 +0530

    Merge pull request #2205 from TejasAmle-Lambdatest/stage
    extended debugging options selenium

--- sidebars.js
+++ sidebars.js
@@ -2409,7 +2409,11 @@ module.exports = {
             type: "category",
             collapsed: true,
             label: "Debugging",
-            items: ["debugging-options", "telemetry-logs"],
+            items: [
+              "debugging-options", 
+              "telemetry-logs",
+              "extended-debugging-options"
+            ],
           },
           {
             type: "category",
