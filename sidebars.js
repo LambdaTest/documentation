96d7053fb803fd31f99bae7a3fcf6f6e83413575
abhishekkumar <abhishekkumar@lambdatest.com>
2025-11-05 21:55:54 +0530

    Add KaneAI GitHub App Integration documentation

--- sidebars.js
+++ sidebars.js
@@ -1214,6 +1214,11 @@ module.exports = {
         ],
       },
       {
+        type: "doc",
+        label: "GitHub App Integration",
+        id: "github-app-integration",
+      },
+      {
         type: "category",
         collapsed: true,
         label: "Knowledge Base",
