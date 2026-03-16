b030f92c19c5752aee78c6123b9199e198a1c694
Isha vyas <72408461+Ishavyas9@users.noreply.github.com>
2026-03-16 16:18:29 +0530

    Merge pull request #2708 from Private1647/stage
    create pr in KaneAI doc

--- sidebars.js
+++ sidebars.js
@@ -1419,6 +1419,7 @@ module.exports = {
           "kane-ai-test-plan",
           "kaneai-hyperexecute-test-run-execution",
           "kaneai-ci-cd-automation",
+          "kaneai-create-pr",
           "test-runs-configurations",
           "kaneai-scheduled-test-runs",
         ],
