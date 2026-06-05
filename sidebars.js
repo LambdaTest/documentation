5270bdeb8af84148a8b18bc08532933318b2c127
Chaitanya Sharma <chaitanyas@LT-MBP-345.local>
2026-06-05 13:42:31 +0530

    Add SmartUI CustomScroll documentation

--- sidebars.js
+++ sidebars.js
@@ -3554,6 +3554,11 @@ module.exports = {
             label: "Handle Sticky Elements",
             id: "smartui-handle-sticky-elements",
           },
+          {
+            type: "doc",
+            label: "CustomScroll Screenshots",
+            id: "smartui-customscroll",
+          },
         ],
       },
 
