9db644bf28aa5426fdf4eac852f8a756d892fb48
salmank <salmank@lambdatest.com>
2026-07-22 17:42:55 +0530

    updated sidebar

--- sidebars.js
+++ sidebars.js
@@ -370,6 +370,11 @@ module.exports = {
           },
           {
             type: "doc",
+            label: "Voice Agent Testing",
+            id: "voice-agent",
+          },
+          {
+            type: "doc",
             label: "Phone Agent Testing",
             id: "phone-agent",
           },
