6e40fce54be704c6c5fa920064c30a45652dd676
Mythili <mythilr@testmuai.com>
2026-07-23 18:40:38 +0530

    smartui agent skills doc

--- sidebars.js
+++ sidebars.js
@@ -3463,6 +3463,7 @@ module.exports = {
         },
         items: [
           "smartui-running-your-first-project",
+          "smartui-agent-skills",
           "smartui-guided-walkthrough",
           "smartui-cli-env-variables",
           "smartui-approval-workflow-guide",
