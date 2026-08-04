95fdd05f68bf2efafaf121b08d2262ba90dde022
Bhawana <bhawana.official01@gmail.com>
2026-08-04 15:10:53 +0530

    kane-cli side bar and llm

--- sidebars.js
+++ sidebars.js
@@ -4373,6 +4373,16 @@ module.exports = {
           },
         ],
       },
+{
+            type: "doc",
+            label: "Use Cases",
+            id: "kane-cli-use-cases",
+          },
+
+
+
+
+
       {
         type: "category",
         collapsed: true,
