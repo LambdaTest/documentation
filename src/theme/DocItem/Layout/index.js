10dad76d9474ea66b4652a59bb5a62e6cbb07a90
salmank <salmank@lambdatest.com>
2026-08-06 12:28:39 +0530

    fixed lt-docs

--- src/theme/DocItem/Layout/index.js
+++ src/theme/DocItem/Layout/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Layout from '@theme-original/DocItem/Layout';
 import CopyPageButton from '@site/src/component/CopyPageButton/CopyPageButton';
+import {useLocation} from '@docusaurus/router';
 import styles from './styles.module.css';
 
+function isDocsHomePage(pathname) {
+  return /^\/support\/docs\/?$/.test(pathname);
+}
+
 export default function LayoutWrapper(props) {
+  const location = useLocation();
+  const hideCopyButton = isDocsHomePage(location.pathname);
+
   return (
     <div className={styles.docLayoutWrapper}>
-      <div className={styles.copyBtnRow}>
-        <CopyPageButton />
-      </div>
+      {!hideCopyButton && (
+        <div className={styles.copyBtnRow}>
+          <CopyPageButton />
+        </div>
+      )}
       <Layout {...props} />
     </div>
   );
--- src/theme/DocItem/Layout/index.tsx
+++ src/theme/DocItem/Layout/index.tsx
@@ -9,8 +9,13 @@ import styles from './styles.module.css';
 
 type Props = WrapperProps<typeof LayoutType>;
 
+function isDocsHomePage(pathname: string): boolean {
+  return /^\/support\/docs\/?$/.test(pathname);
+}
+
 export default function LayoutWrapper(props: Props): ReactNode {
   const location = useLocation();
+  const hideCopyButton = isDocsHomePage(location.pathname);
 
   // Build canonical using testmu.ai + current path
   const canonical = `https://www.testmuai.com${location.pathname}`;
@@ -22,9 +27,11 @@ export default function LayoutWrapper(props: Props): ReactNode {
         </Head>
       )}
       <div className={styles.docLayoutWrapper}>
-        <div className={styles.copyBtnRow}>
-          <CopyPageButton />
-        </div>
+        {!hideCopyButton && (
+          <div className={styles.copyBtnRow}>
+            <CopyPageButton />
+          </div>
+        )}
         <Layout {...props} />
       </div>
     </>
