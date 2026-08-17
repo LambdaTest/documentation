1d2a228be6aa591504fa57b091881ed7d87b6e0b
Ishavyas9 <ishav@lambdatest.com m>
2026-08-17 15:10:38 +0530

    fix video issue

--- custom.js
+++ custom.js
@@ -324,6 +324,7 @@
                   var iframe = document.createElement("iframe");
                   iframe.setAttribute("frameborder", "0");
                   iframe.setAttribute("allowfullscreen", "");
+                  iframe.setAttribute("referrerpolicy", "strict-origin-when-cross-origin");
                   iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
                   this.innerHTML = "";
                   this.appendChild(iframe);
