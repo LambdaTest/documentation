b89a568cc400d49a842532a30cb492db1f9e990a
Ishavyas9 <ishav@lambdatest.com>
2026-09-16 12:26:05 +0530

    docs main page updated

--- src/pages/docs.js
+++ src/pages/docs.js
@@ -120,12 +120,13 @@ export default function Home() {
                 <h2 className="homeMain_h2"><Icon light="hyp-light-icon.svg" dark="hyp-dark-icon.svg" alt="" />HyperExecute</h2>
                 <div className="home_inners">
                   <a href="/support/docs/getting-started-with-hyperexecute/"><p className="p_home_inners">Getting Started</p></a>
+                  <a href="/support/docs/key-features-of-hyperexecute/"><p className="p_home_inners">Features</p></a>
                   <a href="/support/docs/hyperexecute-yaml-parameters/"><p className="p_home_inners">HyperExecute YAML</p></a>
                   <a href="/support/docs/hyperexecute-cli-run-tests-on-hyperexecute-grid/"><p className="p_home_inners">HyperExecute CLI</p></a>
                   <a href="/support/docs/hyperexecute-cli-gui/"><p className="p_home_inners">HyperExecute GUI &nbsp;<NewTag value="BETA" /></p></a>
-                  <a href="/support/docs/hyperexecute-mcp-server/"><p className="p_home_inners">HyperExecute MCP Server</p></a>
-                  <a href="/support/docs/key-features-of-hyperexecute/"><p className="p_home_inners">Features</p></a>
-                  <a href="/support/docs/integration-with-hyperexecute/"><p className="p_home_inners">Integrations</p></a>
+                  <a href="/support/docs/hyperexecute-mcp-server/"><p className="p_home_inners">HyperExecute MCP</p></a>
+                 <a href="/support/docs/hyperexecute-run-jmeter-tests/"><p className="p_home_inners">JMeter Performance Testing</p></a>
+                  <a href="/support/docs/hyperexecute-gattling-testing/"><p className="p_home_inners">Gatling Performance Testing</p></a>
                   <a href="/support/docs/hyperexecute-private-cloud-setup/"><p className="p_home_inners">Private Cloud</p></a>
                 </div>
               </div>
